170b9eda9b47cf5bbdd93a203089fc0480805b6d
Tanner Donovan <ttdonovan@gmail.com>
2014-09-04 18:14:06 -0700

    generate 'movie' resource and update MovieRouter to return dummy model data

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('movies');
+  this.resource('movie', { path: 'movies/:movie_id' });
 
   this.route('not-found', { path: '/*wildcard' });
 });
