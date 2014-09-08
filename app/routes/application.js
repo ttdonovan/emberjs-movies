aeea4af33922030f277ffe4122653b5bc8041672
Tanner Donovan <ttdonovan@gmail.com>
2014-09-07 23:00:00 -0700

    add ApplicationController and keywords

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    search: function(q) {
-      if (q) {
-        this.transitionTo('/search?q=' + q);
+    search: function(keywords) {
+      if (keywords) {
+        this.transitionTo('/search?q=' + keywords);
       }
     }
   }
